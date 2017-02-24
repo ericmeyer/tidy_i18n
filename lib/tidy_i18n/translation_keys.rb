f14aa634176aec29f307fd116ce1916779fff23f
Eric Meyer <emeyer@8thlight.com>
2017-02-24 22:35:58 +0000

    wip

--- lib/tidy_i18n/translation_keys.rb
+++ lib/tidy_i18n/translation_keys.rb
@@ -13,61 +13,88 @@ module TidyI18n
       attr_reader :parsed_keys
 
       def initialize
-        self.key_parts = []
+        # move to start_stream? or even start_document?
+        # self.key_parts = []
         self.parsed_keys = []
       end
 
-      def start_stream(*row); end
-      def start_document(*row); end
+      def start_stream(*row)
+        puts "start_stream"
+      end
+      def start_document(*row)
+        # puts "start_document"
+      end
       def start_sequence(*row)
-        self.current_sequence_key = TranslationKey.new(key_parts.join("."), [])
+        puts "start_sequence"
+        # self.current_sequence_key = TranslationKey.new(key_parts.join("."), [])
       end
 
       def start_mapping(*row)
-        @last_scalar = nil
+        puts "start_mapping: #{row.inspect}"
+        self.current_scalars = []
+        # self.current_key = TranslationKey.new(row.first)
+        # @last_scalar = nil
+
       end
 
       def scalar(*row)
-        current_sequence_key.value << row.first if building_sequence?
-        if leaf?
-          append_parsed_key(row)
-        else
-          key_parts << row.first
-          @last_scalar = row
-        end
+        puts "scalar: #{row.inspect}"
+        self.current_scalars = current_scalars + [row.first]
+        # current_sequence_key.value << row.first if building_sequence?
+        # if leaf?
+        #   append_parsed_key(row)
+        # else
+        #   key_parts << row.first
+        #   @last_scalar = row
+        # end
       end
 
       def end_mapping(*row)
-        @key_parts.pop
+        puts "end_mapping"
+        puts "current_scalars: #{current_scalars.inspect}"
+        current_scalars.each_slice(2) do |(name, value)|
+          puts name + " " + value
+          parsed_keys << TranslationKey.new(name, value)
+        end
+        # if current_scalars.count == 2
+        #   parsed_keys << TranslationKey.new(current_scalars.first, current_scalars.last)
+        #   current_scalars = []
+        # end
+        # @key_parts.pop
       end
 
-      def end_document(*row); end
-      def end_stream(*row); end
+      def end_document(*row)
+        # puts "end_document"
+      end
+      def end_stream(*row)
+        puts "end_stream"
+      end
       def end_sequence
-        self.current_sequence_key = nil
+        puts "end_sequence"
+        # self.current_sequence_key = nil
       end
 
       private
 
-      def append_parsed_key(row)
-        if building_sequence?
-          parsed_keys << current_sequence_key
-        else
-          parsed_keys << TranslationKey.new(key_parts.join("."), row.first)
-        end
-        key_parts.pop
-        @last_scalar = nil
-      end
-
-      def building_sequence?
-        !current_sequence_key.nil?
-      end
-
-      def leaf?
-        @last_scalar
-      end
-
-      attr_accessor :key_parts, :current_sequence_key
+      # def append_parsed_key(row)
+      #   if building_sequence?
+      #     parsed_keys << current_sequence_key
+      #   else
+      #     parsed_keys << TranslationKey.new(key_parts.join("."), row.first)
+      #   end
+      #   key_parts.pop
+      #   @last_scalar = nil
+      # end
+      #
+      # def building_sequence?
+      #   !current_sequence_key.nil?
+      # end
+      #
+      # def leaf?
+      #   @last_scalar
+      # end
+      #
+      attr_accessor :current_scalars
       attr_writer :parsed_keys
 
     end
--- spec/tidy_i18n/translation_keys_spec.rb
+++ spec/tidy_i18n/translation_keys_spec.rb
@@ -6,149 +6,164 @@ describe TidyI18n::TranslationKeys do
     TidyI18n::TranslationKeys.parse("").should == []
   end
 
-  it "has one key for just the locale" do
+  it "has one key with a value" do
     yaml = <<YAML
-en:
+foo: Bar
 YAML
-    keys = TidyI18n::TranslationKeys.parse(yaml)
-    keys.size.should == 1
-    keys.first.name.should == "en"
-    keys.first.value.should == ""
-  end
 
-  it "has one nested key" do
-    yaml = <<YAML
-en:
-  foo: "Bar"
-YAML
     keys = TidyI18n::TranslationKeys.parse(yaml)
+
     keys.size.should == 1
-    keys.first.name.should == "en.foo"
+    keys.first.name.should == "foo"
     keys.first.value.should == "Bar"
   end
 
-  it "has two keys at the same level" do
+  it "has two top level keys" do
     yaml = <<YAML
-en:
-  foo: "FOO"
-  bar: "BAR"
+foo: Foo
+bar: Bar
 YAML
+
     keys = TidyI18n::TranslationKeys.parse(yaml)
+
     keys.size.should == 2
-    keys.first.name.should == "en.foo"
-    keys.first.value.should == "FOO"
-    keys.last.name.should == "en.bar"
-    keys.last.value.should == "BAR"
+    keys.first.should == TidyI18n::TranslationKey.new("foo", "Foo")
+    keys.last.should == TidyI18n::TranslationKey.new("bar", "Bar")
   end
 
-  it "has one key nested mutliple times" do
-    yaml = <<YAML
-en:
-  foo:
-    bar:
-      baz: "123"
-YAML
-    keys = TidyI18n::TranslationKeys.parse(yaml)
-    keys.size.should == 1
-    keys.first.name.should == "en.foo.bar.baz"
-    keys.first.value.should == "123"
-  end
-
-  it "has keys for a complicated example" do
-    yaml = <<YAML
-en:
-  foo:
-    bar: 123
-    baz: 456
-    quo:
-      wat: What
-  foo2: Wat
-YAML
-    keys = TidyI18n::TranslationKeys.parse(yaml)
-    keys.size.should == 4
-    keys.map(&:name).should =~ [
-      "en.foo.bar",
-      "en.foo.baz",
-      "en.foo.quo.wat",
-      "en.foo2"
-    ]
-  end
-
-  it "includes a key twice" do
-    yaml = <<YAML
-en:
-  foo: Foo1
-  foo: Foo2
-YAML
-    keys = TidyI18n::TranslationKeys.parse(yaml)
-    keys.size.should == 2
-    keys.map(&:name).should == ["en.foo", "en.foo"]
-    keys.map(&:value).should =~ ["Foo1", "Foo2"]
-  end
-
-  describe "Parsing sequences" do
-    it "parses the only sequence when it has one element" do
-      yaml = <<YAML
-en:
-  day_names:
-    - Monday
-YAML
-
-      keys = TidyI18n::TranslationKeys.parse(yaml)
-
-      keys.size.should == 1
-      keys.first.name.should == "en.day_names"
-      keys.first.value.should == ["Monday"]
-    end
-
-    it "has a sequence followed by another key" do
-      yaml = <<YAML
-en:
-  day_names:
-    - Monday
-  foo: "Bar"
-YAML
-
-      keys = TidyI18n::TranslationKeys.parse(yaml)
-
-      keys.size.should == 2
-      keys.first.name.should == "en.day_names"
-      keys.first.value.should == ["Monday"]
-      keys.last.name.should == "en.foo"
-      keys.last.value.should == "Bar"
-    end
-
-    it "has a two elements sequence" do
-      yaml = <<YAML
-en:
-  day_names:
-    - Monday
-    - Tuesday
-YAML
-
-      keys = TidyI18n::TranslationKeys.parse(yaml)
-
-      keys.size.should == 1
-      keys.first.name.should == "en.day_names"
-      keys.first.value.should == ["Monday", "Tuesday"]
-    end
-
-    xit "has a two elements sequence followed by another key" do
-      yaml = <<YAML
-en:
-  day_names:
-    - Monday
-    - Tuesday
-  foo: "Bar"
-YAML
-
-      keys = TidyI18n::TranslationKeys.parse(yaml)
-
-      keys.size.should == 2
-      keys.first.name.should == "en.day_names"
-      keys.first.value.should == ["Monday", "Tuesday"]
-      keys.last.name.should == "en.foo"
-      keys.last.value.should == "Bar"
-    end
-  end
+#   it "has one nested key" do
+#     yaml = <<YAML
+# en:
+#   foo: "Bar"
+# YAML
+#     keys = TidyI18n::TranslationKeys.parse(yaml)
+#     keys.size.should == 1
+#     keys.first.name.should == "en.foo"
+#     keys.first.value.should == "Bar"
+#   end
+#
+#   xit "has two keys at the same level" do
+#     yaml = <<YAML
+# en:
+#   foo: "FOO"
+#   bar: "BAR"
+# YAML
+#     keys = TidyI18n::TranslationKeys.parse(yaml)
+#     keys.size.should == 2
+#     keys.first.name.should == "en.foo"
+#     keys.first.value.should == "FOO"
+#     keys.last.name.should == "en.bar"
+#     keys.last.value.should == "BAR"
+#   end
+#
+#   xit "has one key nested mutliple times" do
+#     yaml = <<YAML
+# en:
+#   foo:
+#     bar:
+#       baz: "123"
+# YAML
+#     keys = TidyI18n::TranslationKeys.parse(yaml)
+#     keys.size.should == 1
+#     keys.first.name.should == "en.foo.bar.baz"
+#     keys.first.value.should == "123"
+#   end
+#
+#   xit "has keys for a complicated example" do
+#     yaml = <<YAML
+# en:
+#   foo:
+#     bar: 123
+#     baz: 456
+#     quo:
+#       wat: What
+#   foo2: Wat
+# YAML
+#     keys = TidyI18n::TranslationKeys.parse(yaml)
+#     keys.size.should == 4
+#     keys.map(&:name).should =~ [
+#       "en.foo.bar",
+#       "en.foo.baz",
+#       "en.foo.quo.wat",
+#       "en.foo2"
+#     ]
+#   end
+#
+#   xit "includes a key twice" do
+#     yaml = <<YAML
+# en:
+#   foo: Foo1
+#   foo: Foo2
+# YAML
+#     keys = TidyI18n::TranslationKeys.parse(yaml)
+#     keys.size.should == 2
+#     keys.map(&:name).should == ["en.foo", "en.foo"]
+#     keys.map(&:value).should =~ ["Foo1", "Foo2"]
+#   end
+#
+#   describe "Parsing sequences" do
+#     xit "parses the only sequence when it has one element" do
+#       yaml = <<YAML
+# en:
+#   day_names:
+#     - Monday
+# YAML
+#
+#       keys = TidyI18n::TranslationKeys.parse(yaml)
+#
+#       keys.size.should == 1
+#       keys.first.name.should == "en.day_names"
+#       keys.first.value.should == ["Monday"]
+#     end
+#
+#     xit "has a sequence followed by another key" do
+#       yaml = <<YAML
+# en:
+#   day_names:
+#     - Monday
+#   foo: "Bar"
+# YAML
+#
+#       keys = TidyI18n::TranslationKeys.parse(yaml)
+#
+#       keys.size.should == 2
+#       keys.first.name.should == "en.day_names"
+#       keys.first.value.should == ["Monday"]
+#       keys.last.name.should == "en.foo"
+#       keys.last.value.should == "Bar"
+#     end
+#
+#     xit "has a two elements sequence" do
+#       yaml = <<YAML
+# en:
+#   day_names:
+#     - Monday
+#     - Tuesday
+# YAML
+#
+#       keys = TidyI18n::TranslationKeys.parse(yaml)
+#
+#       keys.size.should == 1
+#       keys.first.name.should == "en.day_names"
+#       keys.first.value.should == ["Monday", "Tuesday"]
+#     end
+#
+#     xit "has a two elements sequence followed by another key" do
+#       yaml = <<YAML
+# en:
+#   day_names:
+#     - Monday
+#     - Tuesday
+#   foo: "Bar"
+# YAML
+#
+#       keys = TidyI18n::TranslationKeys.parse(yaml)
+#
+#       keys.size.should == 2
+#       keys.first.name.should == "en.day_names"
+#       keys.first.value.should == ["Monday", "Tuesday"]
+#       keys.last.name.should == "en.foo"
+#       keys.last.value.should == "Bar"
+#     end
+#   end
 end
